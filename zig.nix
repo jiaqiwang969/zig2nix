8e2fd0b8444d74804372ad4fb8013fab434fd3a7
jiaqiwang969 <jiaqiwang969@gmail.com>
2025-02-12 00:28:30 -0800

    jq: downgrade the llvm to 13

--- zig.nix
+++ zig.nix
@@ -11,7 +11,7 @@
   , fetchurl
   , zig-shell-completions
   , cmake
-  , llvmPackages_19
+  , llvmPackages_13
   , libxml2
   , zlib
   , coreutils
@@ -34,7 +34,7 @@ let
     changelog = release.notes;
   };
 in {
-  src = with llvmPackages_19; stdenv.mkDerivation (finalAttrs: {
+  src = with llvmPackages_13; stdenv.mkDerivation (finalAttrs: {
     pname = "zig";
     inherit version;
 
